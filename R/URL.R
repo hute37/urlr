fb03da02a089dabddeb2ceb82843e0bb2d827c49
G Pelosi <hute37@gmail.com>
2016-01-27 11:27:11 +0100

    scalar create

--- R/URL.R
+++ R/URL.R
@@ -1,46 +1,99 @@
 #' Create a URL S3 object
 #'
 #' @param url string url rapresentation
+#' @param base a base url for relative urls (string or URL)
 #' @param src an URL object fron which this object is derived
 #' @param meta an optional array of meta attributes
 #' @param ... other arguments passed to specific methods
 #' @return an URL object
 #' @export
 #'
-URL <- function(url, src, meta=c(), ...) {
-    if (missing(url)) return(NULL)
-    if (is.null(url)) return(NULL)
-    if (is.na(url)) return(NA_character_) # TODO(gp): NA_URL
+URL <- function(url, base = paste0("file://",getwd()), src = NULL, meta = c(), ...) {
+  if (missing(url) || is.null(url))
+    return(NULL)
+  if (is.URL(url)) {
+    return(URL(url$url,src=ifelse(is.null(src), url,src)))
+  }
+  if (is.list(url)) {
+    return(lapply(url, function(u)
+      URL(u, base, src, meta, ...)))
+  }
 
-    comp <- urltools::url_parse(url)
+  if (!is.null(src)) {
+    if (!is.URL(src)) {
+      stopifnot(is.character(src))
+      src <- URL(src)
+    }
+  }
+  url <- as.character(url)
+  rel <- NULL
+  schemeless <- !grepl('^[a-z]+:',url)
+  relative <- schemeless && (substr(url,1,1) != "/")
+  if (schemeless) {
+    if (relative) {
+      if (!is.URL(base)) {
+        base <- URL(base)
+      }
+      rel <- url
+      url <- paste(base$url,rel, sep="/") # TODO(gp): url canon
 
-    if (missing(src)) {
-      origin <- TRUE
-      src <- NULL
-      env <- new.env()
     } else {
-      origin <- FALSE
-      env <- new.env(parent = src$env)
-
+      base <- NULL
+      url <- paste0("file://", url)
     }
 
-    x <- structure(list(
-      url = url,
-      comp = comp,
-      src = src,
-      env = env,
-      origin = origin
-    ),class = c(paste("URL",comp$scheme,sep="_"), "URL"))
+  } else {
+    base <- NULL
+  }
 
 
-    x <- URL_init(x, meta=meta, ...) # warning StacKOverflow
-    x <- URL_meta(x, meta=meta)
+  comp <- urltools::url_parse(url)
+
+  if (is.null(src)) {
+    origin <- TRUE
+    src <- NULL
+    env <- new.env()
+  } else {
+    origin <- FALSE
+    env <- new.env(parent = src$env)
+  }
+
+  x <- structure(list(
+    url = url,
+    comp = comp,
+    src = src,
+    base = base,
+    rel = rel,
+    env = env,
+    relative = relative,
+    origin = origin
+  ),class = c(paste("URL",comp$scheme,sep = "_"), "URL"))
+
+
+  x <- URL_init(x, meta = meta, ...) # warning StacKOverflow
+  x <- URL_meta(x, meta = meta)
 }
+
+#' check if URL object
+#'
+#' @param x an object to check
+#' @return check condition
+#' @export
+is.URL <- function(x)
+  inherits(x, "URL")
+
 
 #' @export
 #' @method as.character URL
-#'
-as.character.URL <- function(x, ...) x$url
+as.character.URL <- function(x, ...) {
+  if (missing(x) || is.null(x))
+    return(NULL)
+  if (is.URL(x))
+    return(x$url)
+  if (is.list(x))
+    return(lapply(x, as.character.URL))
+  stopifnot(is.URL(x))
+}
 
 #' conver string form of object to URL object
 #'
@@ -50,16 +103,19 @@ as.character.URL <- function(x, ...) x$url
 #' @return an URL object
 #' @export
 #'
-as.URL <- function(x, ...) UseMethod("as.URL")
+as.URL <- function(x, ...)
+  UseMethod("as.URL")
 
 #' @rdname as.URL
 #' @export
 #' @method as.URL default
 #'
-as.URL.default <- base::Vectorize(FUN=function(x, ...) {
-  url <- as.character(x)
-  x <- URL(url, ...)
-})
+as.URL.default <- base::Vectorize(
+  FUN = function(x, ...) {
+    url <- as.character(x)
+    x <- URL(url, ...)
+  }
+)
 
 
 
@@ -75,15 +131,16 @@ as.URL.default <- base::Vectorize(FUN=function(x, ...) {
 #' @return raises a stop condition
 #' @export
 #'
-URL_stop <- function(x, caller, text, ...) UseMethod("URL_stop")
+URL_stop <- function(x, caller, text, ...)
+  UseMethod("URL_stop")
 
 #' @rdname URL_stop
 #' @export
 #' @method URL_stop default
 #'
 URL_stop.default <- function(x, caller, text, ...) {
-    m <- paste(c("method:",caller,text,"URL:",x$url))
-    stop(m)
+  m <- paste(c("method:",caller,text,"URL:",x$url))
+  stop(m)
 }
 
 
@@ -96,14 +153,16 @@ URL_stop.default <- function(x, caller, text, ...) {
 #' @return raises a stop condition
 #' @export
 #'
-URL_stop_unsupported <- function(x, caller, ...) UseMethod("URL_stop_unsupported")
+URL_stop_unsupported <-
+  function(x, caller, ...)
+    UseMethod("URL_stop_unsupported")
 
 #' @rdname URL_stop_unsupported
 #' @export
 #' @method URL_stop_unsupported default
 #'
 URL_stop_unsupported.default <- function(x, caller, ...) {
-    URL_stop(x,caller=caller, text="unsupported operation", ...)
+  URL_stop(x,caller = caller, text = "unsupported operation", ...)
 }
 
 
@@ -116,13 +175,15 @@ URL_stop_unsupported.default <- function(x, caller, ...) {
 #' @return an initialized URL obect
 #' @export
 #'
-URL_init <- function(x, meta=c(), ...) UseMethod("URL_init")
+URL_init <- function(x, meta = c(), ...)
+  UseMethod("URL_init")
 
 #' @rdname URL_init
 #' @export
 #' @method URL_init default
 #'
-URL_init.default <- function(x, meta=c(), ...) x
+URL_init.default <- function(x, meta = c(), ...)
+  x
 
 #' set meta named array argument
 #' as URL object attributes (with prefix 'meta.')
@@ -134,13 +195,15 @@ URL_init.default <- function(x, meta=c(), ...) x
 #' @return a 'meta' decorated URL obect
 #' @export
 #'
-URL_meta <- function(x, meta=c(), ...) UseMethod("URL_meta")
+URL_meta <- function(x, meta = c(), ...)
+  UseMethod("URL_meta")
 
 #' @rdname URL_meta
 #' @export
 #' @method URL_meta default
 #'
-URL_meta.default <- function(x, meta=c(), ...) x
+URL_meta.default <- function(x, meta = c(), ...)
+  x
 
 
 
@@ -159,16 +222,16 @@ URL_meta.default <- function(x, meta=c(), ...) x
 #' @export
 #'
 as.conn.url <- function(x, open = "", blocking = TRUE,
-    encoding = getOption("encoding"), method, ...) UseMethod("as.conn.url")
+                        encoding = getOption("encoding"), method, ...)
+  UseMethod("as.conn.url")
 
 #' @rdname as.conn.url
 #' @export
 #' @method as.conn.url default
 #'
 as.conn.url.default <- function(x, open = "", blocking = TRUE,
-                           encoding = getOption("encoding"), method, ...) {
-
-    URL_stop_unsupported(x,'as.conn.url')
+                                encoding = getOption("encoding"), method, ...) {
+  URL_stop_unsupported(x,'as.conn.url')
 }
 
 
@@ -182,13 +245,15 @@ as.conn.url.default <- function(x, open = "", blocking = TRUE,
 #' @return a local file:// URL obect or na:// URL in case of failure
 #' @export
 #'
-URL_fetch <- function(x, ...) UseMethod("URL_fetch")
+URL_fetch <- function(x, ...)
+  UseMethod("URL_fetch")
 
 #' @rdname URL_fetch
 #' @export
 #' @method URL_fetch default
 #'
-URL_fetch.default <- function(x, ...) x
+URL_fetch.default <- function(x, ...)
+  x
 
 #' follows 'R data()' and httr euristics to build a data rapresentation of an URL
 #' the object returned is data.frame or environment
@@ -201,13 +266,16 @@ URL_fetch.default <- function(x, ...) x
 #' @return an list of URL with relerence to internal environment for data object
 #' @export
 #'
-URL_parse <- function(x, ...) UseMethod("URL_parse")
+URL_parse <- function(x, ...)
+  UseMethod("URL_parse")
 
 #' @rdname URL_parse
 #' @export
 #' @method URL_parse default
 #'
-URL_parse.default <- function(x, ...) URL_stop_unsupported(x,'URL_parse')
+URL_parse.default <-
+  function(x, ...)
+    URL_stop_unsupported(x,'URL_parse')
 
 
 
@@ -221,11 +289,13 @@ URL_parse.default <- function(x, ...) URL_stop_unsupported(x,'URL_parse')
 #' @return an parsed data object
 #' @export
 #'
-URL_data <- function(x, ...) UseMethod("URL_data")
+URL_data <- function(x, ...)
+  UseMethod("URL_data")
 
 #' @rdname URL_data
 #' @export
 #' @method URL_data default
 #'
-URL_data.default <- function(x, ...) URL_stop_unsupported(x,'URL_data')
-
+URL_data.default <-
+  function(x, ...)
+    URL_stop_unsupported(x,'URL_data')
--- tests/testthat/test-create.R
+++ tests/testthat/test-create.R
@@ -3,6 +3,108 @@ context("create")
 ## TODO: Rename context
 ## TODO: Add more tests
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("absolute scalar", {
+
+  url <- "http://www.w3c.org"
+  u <- URL(url)
+
+  expect_equal(u$url, url)
+  expect_equal(as.character(u), url)
+  expect_false(u$relative)
+  expect_true(u$origin)
+
 })
+
+
+test_that("relative, base $(pwd)", {
+
+  x <- URL("README.md")
+
+  expect_equal(x$url, paste0("file://",getwd(),"/","README.md"))
+  expect_match(as.character(x), '^file:///.*/README.md')
+  expect_true(x$relative)
+  expect_true(x$origin)
+
+})
+
+
+test_that("absolute, no schema", {
+
+  x <- URL("/etc/hosts")
+
+  expect_equal(x$url, "file:///etc/hosts")
+  expect_false(x$relative)
+  expect_true(x$origin)
+
+})
+
+test_that("relative, local path", {
+
+  x <- URL("hosts", base="/etc")
+
+  expect_equal(x$url, "file:///etc/hosts")
+  expect_true(x$relative)
+  expect_true(x$origin)
+
+})
+
+
+test_that("relative, remote path", {
+
+  u <- URL("index.html", base="http://www.w3c.org")
+
+  expect_equal(as.character(u), "http://www.w3c.org/index.html")
+  expect_true(u$relative)
+  expect_true(u$origin)
+
+})
+
+
+test_that("absolute scalar with source", {
+
+  u.url <- "http://www.w3c.org"
+  u <- URL(u.url)
+
+  v.url <- "http://ietf.org"
+  v <- URL(v.url, src=u)
+
+  expect_equal(v$url, v.url)
+  expect_equivalent(v$src, u)
+  expect_false(v$origin)
+  expect_true(v$src$origin)
+
+})
+
+
+test_that("absolute scalar with source string", {
+
+  u.url <- "http://www.w3c.org"
+  v.url <- "http://ietf.org"
+
+  v <- URL(v.url, src=u.url)
+
+  expect_equal(v$url, v.url)
+  expect_equal(v$src$url, u.url)
+  expect_false(v$origin)
+  expect_true(v$src$origin)
+
+})
+
+test_that("absolute scalar with 3 source levels", {
+
+  u.url <- "http://www.w3c.org"
+  v.url <- "http://ietf.org"
+  w.url <- "http://iana.org"
+
+  v <- URL(v.url, src=u.url)
+  w <- URL(w.url, src=v)
+
+  expect_equal(w$url, w.url)
+  expect_equal(w$src$url, v.url)
+  expect_equal(w$src$src$url, u.url)
+  expect_false(w$origin)
+  expect_false(w$src$origin)
+  expect_true(w$src$src$origin)
+
+})
+
